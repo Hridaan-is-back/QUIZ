91c9d8fa4df76fd083bb11475f88b9fa7877129c
Hridaan-is-back <81317425+Hridaan-is-back@users.noreply.github.com>
2021-08-03 17:12:18 +0530

    Add files via upload

---
+++ Class-89/guess_the_word_game/ADV-C90-Student/activity2.js
@@ -0,0 +1,9 @@
+function Back() {
+windown.location="activity_1.html";
+}
+
+function getScore() 
+{
+score=localStorage.getItem("score");
+document.getElementById("update").innerHTML="<h1>Score: "+ score + "<h1>";
+}
